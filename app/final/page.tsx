433528eca8e0dccfd31bdffbce85ee20e7869ada
Kaye Chan <kayemychan@gmail.com>
2024-12-10 00:58:41 +0000

    styled form

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -4,9 +4,10 @@ export default function Page() {
       <h1 className="text-3xl">Final Exam</h1>
       <form>
         <label htmlFor="product">Product Description</label>
-        <input type="text" name="product" id="product" className="text-black
+        <input type="text" name="product" id="product" className="m-5 text-black
         bg-yellow-400"/>
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold 
+        <br />
+        <button className="ml-10 bg-blue-500 hover:bg-blue-400 text-white font-bold 
         py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" 
         type="submit">Save</button>
       </form>
